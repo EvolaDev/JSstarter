fca2cea3b665dacb43bc408defc3960999fb2753
EvolaDev <denis.abrahin1996@gmail.com>
2020-04-01 22:14:08 +0300

    add behaviour patterns

---
+++ patterns/behaviour/mediator.js
@@ -0,0 +1,47 @@
+class User {
+    constructor(name) {
+        this.name = name;
+        this.room = null;
+    }
+
+    send(message, to) {
+        this.room.send(message, this, to);
+    }
+
+    receive(message, from) {
+        console.log(`${from.name} => ${this.name} : ${message}`);
+    }
+}
+
+class ChatRoom {
+    constructor() {
+        this.users = {};
+    }
+
+    register(user) {
+        this.users[user.name] = user;
+        user.room = this;
+    }
+
+    send(message, from, to) {
+        if (to) {
+            to.receive(message, from);
+        } else {
+            Object.keys(this.users).forEach(key => {
+                if (this.users[key] !== from) {
+                    this.users[key].receive(message, from);
+                }
+            });
+        }
+    }
+}
+
+const vlad = new User('Disa');
+const kate = new User('Kate');
+
+const room = new ChatRoom();
+
+room.register(vlad);
+room.register(kate);
+
+vlad.send('Hello', kate);
